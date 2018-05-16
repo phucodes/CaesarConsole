ef9a9d75598ca90f7805559b26e4435b63524c14
phudinhn <phudinhn@gmail.com>
2018-05-16 17:04:27 -0500

    Add project files.

---
+++ CaesarConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CaesarConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your input here: ");
+            string inputMessage = Console.ReadLine();
+            string outputMessage = RotCaesar(inputMessage);
+            Console.WriteLine("Rotated by 13 is: " +outputMessage);
+            Console.ReadLine();
+        }
+
+        private static string RotCaesar(string inputMessage)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            int char_position;
+
+            for (int i = 0; i < inputMessage.Length; i++)
+            {
+
+            }
+        }
+    }
+}
