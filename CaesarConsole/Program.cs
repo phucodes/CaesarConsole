a4bb217900f777dfde067a9f13e1d3a3f04b381c
phudinhn <phudinhn@gmail.com>
2018-05-17 00:32:57 -0500

    Update Rot13 function

--- CaesarConsole/Program.cs
+++ CaesarConsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace CaesarConsole
 {
@@ -9,24 +11,39 @@ namespace CaesarConsole
         {
             Console.WriteLine("Enter your input here: ");
             string inputMessage = Console.ReadLine();
-            //string outputMessage = RotCaesar(inputMessage);
-            //Console.WriteLine("Rotated by 13 is: " +outputMessage);
+            // string outputMessage = RotCaesar(inputMessage);
+            // Console.WriteLine("Rotated by 13 is: " +outputMessage);
             Console.ReadLine();
         }
 
-        public static int RotCaesar(string inputMessage)
+        public static string RotCaesar(string inputMessage)
         {
             string alphabet = "abcdefghijklmnopqrstuvwxyz";
-            int char_position = 0;
+            int char_pos = 0;
+            int inputLength = inputMessage.Length;
+            string [] char_arr = new string[inputLength]; // initialize an array same length as that of inputMessage
+            //string empty = "";
 
-            foreach (char characters in inputMessage)
+            for (int i = 0; i < inputMessage.Length; i++) // loop through input message
             {
-                char_position += alphabet.IndexOf(characters);
+                if (alphabet.Contains(inputMessage[i])) // check if input contains characters from alphabet
+                {
+                    // return character index in the alphabet
+                    char_pos = alphabet.IndexOf(inputMessage[i]);
+                }   //TODO: else ignore said char
             }
 
-            Console.WriteLine(char_position);
-            Console.ReadLine();
-            return char_position;
+            int char_rot = char_pos + 13; // rotated character's index by 13
+
+            if (char_rot < 26) // less than total amount of characters in English alphabet
+            {
+                char rotated_char = alphabet[char_rot]; // return rotated characters index in English alphabet
+            }
+
+            else // wrap text from beginning of alphabet again
+            {
+                char rotated_char = alphabet[char_rot % 26];  // TODO: add rotated to string for output
+            }
         }
     }
 }
