fb070f992ef52b098e51e1ab3daa5ee5191e5d7d
phudinhn <phudinhn@gmail.com>
2018-05-17 16:49:10 -0500

    Rework rot13 function.

--- CaesarConsole/Program.cs
+++ CaesarConsole/Program.cs
@@ -20,40 +20,37 @@ namespace CaesarConsole
         public static string RotCaesar(string inputMessage)
         {
             string alphabet = "abcdefghijklmnopqrstuvwxyz";
-            int char_pos = 0;
+            List<int> char_pos = new List<int>();
             char rotated_char;
-            //int inputLength = inputMessage.Length;
-            //string [] char_arr = new string[inputLength]; // initialize an array same length as that of inputMessage
             List<char> char_arr = new List<char>();
-            //string empty = "";
 
             for (int i = 0; i < inputMessage.Length; i++) // loop through input message
             {
                 if (alphabet.Contains(inputMessage[i])) // check if input contains characters from alphabet
                 {
                     // return character index in the alphabet
-                    char_pos = alphabet.IndexOf(inputMessage[i]);
+                    char_pos.Add(alphabet.IndexOf(inputMessage[i]));
                 }   //TODO: else ignore said char
+                Console.WriteLine(char_pos[i]);
             }
-
-            int char_rot = char_pos + 13; // rotated character's index by 13
-
-            if (char_rot < 26) // less than total amount of characters in English alphabet
+            
+            foreach (int chars in char_pos)
             {
-                rotated_char = alphabet[char_rot]; // return rotated characters index in English alphabet
-            }
+                int char_rot = char_pos[chars] + 13; // TODO: FIX INDEX OUT OF RANGE
 
-            else // wrap text from beginning of alphabet again
-            {
-                rotated_char = alphabet[char_rot % 26];  // TODO: add rotated to string for output
-            }
+                if (char_rot < 26)
+                {
+                    rotated_char = alphabet[char_rot];
+                    char_arr.Add(rotated_char);
+                }
 
-            for (int j = 0; j < char_arr.Count; j++)
-            {
-                char_arr.Add(rotated_char); // add rotated characters to List of chars
+                else
+                {
+                    rotated_char = alphabet[char_rot % 26];
+                    char_arr.Add(rotated_char);
+                }
             }
-
-            string ReturnString = string.Join("*", char_arr.ToArray());
+            string ReturnString = string.Join("", char_arr.ToArray());
 
             return ReturnString;
         }
