acf06f9ab59800d59b64adc6216b3069b3baa7f9
phudinhn <phudinhn@gmail.com>
2018-05-17 21:56:31 -0500

    Add input filter.

--- CaesarConsole/Program.cs
+++ CaesarConsole/Program.cs
@@ -22,21 +22,22 @@ namespace CaesarConsole
             List<int> char_pos = new List<int>();
             List<char> char_arr = new List<char>();
             List<int> char_rot = new List<int>();
-            char rotated_char;
+            char rotated_char; // TODO: change input function to ignore space and nonalpha characters
 
             for (int i = 0; i < inputMessage.Length; i++) // loop through input message
             {
-                if (alphabet.Contains(inputMessage[i])) // check if input contains characters from alphabet
+                if (alphabet.Contains(inputMessage[i]) && Char.IsLetter(inputMessage[i])) // check if input contains characters from alphabet
                 {
                     // return character index in the alphabet
-                    char_pos.Add(alphabet.IndexOf(inputMessage[i]));
-                }   //TODO: else ignore said char FIXED
-                //Console.WriteLine(char_pos[i]);
+                    char_pos.Add(alphabet.IndexOf(inputMessage[i])); // add to list of initial index positions
+                }   //TODO: else ignore said char
+                //else
+                //    char_arr.Insert(i, inputMessage[i]);
             }
             
             for (int j = 0; j < char_pos.Count; j++)
             {
-                char_rot.Add(char_pos[j] + 13);
+                char_rot.Add(char_pos[j] + 13); // add to list of rotated position
             }
 
             for (int i = 0; i < char_rot.Count; i++) {
@@ -44,16 +45,16 @@ namespace CaesarConsole
                 if (char_rot[i] < 26)
                 {
                     rotated_char = alphabet[char_rot[i]];
-                    char_arr.Add(rotated_char);
+                    char_arr.Add(rotated_char); // add to list of strings
                 }
 
-                else
+                else // if rotated characters is beyond alphabet, wrap it around
                 {
                     rotated_char = alphabet[char_rot[i] % 26];
                     char_arr.Add(rotated_char);
                 }
             }
-            string ReturnString = string.Join("", char_arr.ToArray());
+            string ReturnString = string.Join("", char_arr.ToArray()); // build output string
 
             return ReturnString;
         }
